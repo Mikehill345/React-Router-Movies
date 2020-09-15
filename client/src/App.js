003db587485da00d0e9bae9f1500d9eaab23808e
mike hill <michael-hill@lambdastudents.com>
2020-09-14 20:21:17 -0400

    finished all questions no stretch

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-
+import { Route, Switch } from 'react-router-dom'
 import SavedList from './Movies/SavedList';
+import MovieList from './Movies/MovieList'
+import Movie from './Movies/Movie'
 
 export default function App () {
   const [saved, setSaved] = useState([]); // Stretch: the ids of "saved" movies
@@ -12,6 +14,7 @@ export default function App () {
       axios
         .get('http://localhost:5000/api/movies') // Study this endpoint with Postman
         .then(response => {
+          setMovieList(response.data)
           // Study this response with a breakpoint or log statements
           // and set the response data as the 'movieList' slice of state
         })
@@ -21,7 +24,7 @@ export default function App () {
     }
     getMovies();
   }, []);
-
+ 
   const addToSavedList = id => {
     // This is stretch. Prevent the same movie from being "saved" more than once
   };
@@ -29,8 +32,17 @@ export default function App () {
   return (
     <div>
       <SavedList list={[ /* This is stretch */]} />
+      <Switch>
 
-      <div>Replace this Div with your Routes</div>
+        <Route path='/movie/:id'>
+          <Movie movies={movieList} />
+        </Route>
+
+        <Route exact path='/'>
+          <MovieList movies={movieList} />
+        </Route>
+
+      </Switch>
     </div>
   );
 }
